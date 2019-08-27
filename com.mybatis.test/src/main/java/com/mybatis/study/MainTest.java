0d2710dd973db490162c203b72e15456a0d7b603
quanying <903953247@qq.com>
2019-08-27 23:03:05 +0800

    feat: add test-module

---
+++ com.mybatis.test/src/main/java/com/mybatis/study/MainTest.java
@@ -0,0 +1,10 @@
+package com.mybatis.study;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+
+public class MainTest {
+  public static void main(String[] args) {
+    System.out.println("hello");
+
+  }
+}
