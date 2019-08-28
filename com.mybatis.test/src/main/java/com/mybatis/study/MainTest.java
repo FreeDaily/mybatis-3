3347b840ea42087a0f1e6d76181d0d979c6844ad
luoquanying <luoquanying4445@haixue.com>
2019-08-28 16:03:49 +0800

    feat: log4j

--- com.mybatis.test/src/main/java/com/mybatis/study/MainTest.java
+++ com.mybatis.test/src/main/java/com/mybatis/study/MainTest.java
@@ -7,22 +7,25 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 
 @Slf4j
 public class MainTest {
-  public static void main(String[] args) throws Exception {
 
-    log.info("start .....");
-    String resource = "mybatis-config.xml";
-    InputStream inputStream = Resources.getResourceAsStream(resource);
-    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    public static void main(String[] args) throws Exception {
 
-    try (SqlSession session = sqlSessionFactory.openSession()) {
-      UserDao mapper = session.getMapper(UserDao.class);
-      User user = mapper.findById(1L);
-      log.info("{}",user);
+        log.info("start .....");
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            UserDao mapper = session.getMapper(UserDao.class);
+            User user = mapper.findById(1L);
+            log.info("{}", user);
+        }
     }
-  }
 }
--- com.mybatis.test/src/main/java/com/mybatis/study/biz/User.java
+++ com.mybatis.test/src/main/java/com/mybatis/study/biz/User.java
@@ -5,5 +5,7 @@ import lombok.Data;
 @Data
 public class User {
 
-  private Long id;
+    private Long id;
+
+    private String name;
 }
